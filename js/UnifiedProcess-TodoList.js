8b5b567854f6fd5a69de081385b4c6a04699fc5b
kimsunin <kimsunin@huniverse.co.kr>
2022-02-23 01:25:21 +0900

    todolist ui 추가

--- js/UnifiedProcess-TodoList.js
+++ js/UnifiedProcess-TodoList.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', async function () {
     createGrid();
     createTodoListGrid();
+    createAnsGrid();
 });
 
 let container, provider, gridView;
@@ -28,7 +29,7 @@ let fields = [
 
 let columns = [
 	{fieldName:"PROC_ID", name:"PROC_ID", width:65, header:{text:"프로세스ID" }, footer: {expression: "count"},styleName: "textLeft"},
-	{fieldName:"PROC_NM", name:"PROC_NM", width:210, header:{text:"프로세스명" }, styleName: "textLeft", popupMenu: "menu1",button: "popup"},
+	{fieldName:"PROC_NM", name:"PROC_NM", width:210, header:{text:"프로세스명" }, styleName: "textLeft"},
     /* 하위 컬럼 display false */ 
 	{fieldName:"PROC_CTN", name:"PROC_CTN", width:300, header:{text:"프로세스 내용" }, styleName: "textLeft",
 		renderer:{
@@ -129,6 +130,24 @@ function createGrid() {
 
 
 /* **************************************************************************************************** */
+const { Editor } = toastui;
+const { colorSyntax } = Editor.plugin;
+
+// const toDoAskEditor = new toastui.Editor({
+//     el: document.querySelector('#toDoAskEditor'),
+//     height: '100%',
+//     initialEditType : 'wysiwyg',
+//     hideModeSwitch : true,
+//     plugins: [colorSyntax],
+// });
+// toDoAskEditor.removeToolbarItem('image');
+
+const toDoAskEditor = toastui.Editor.factory({
+	el: document.querySelector("#toDoAskEditor"),
+	viewer: true,
+	height: '100%',
+});
+
 // ToDoList Grid
 let toDoContainer, toDoProvider, toDoGridView;
 
@@ -137,7 +156,7 @@ let toDoFields = [
     {fieldName:"PROC_NM", dataType:"text"},      // 프로세스 명
     {fieldName:"STEP_ID", dataType:"text"},      // No
     {fieldName:"STEP_NM", dataType:"text"},      // 단계ID
-    {fieldName:"FRST_RGST_DT", dataType:"text"}, // 등록일 
+    {fieldName:"FRST_RGST_DT", dataType:"date"}, // 등록일 
     {fieldName:"COMPL_ST", dataType:"text"},     // 완료여부
     {fieldName:"WR_UNIT", dataType:"text"},      // 업무구분
     {fieldName:"TODO_ASK", dataType:"text"},     // 의뢰내용
@@ -205,9 +224,40 @@ function createTodoListGrid() {
     toDoGridView.columnByName("LAST_UPDT_IP").visible =  colVisible; 
     toDoGridView.columnByName("LAST_UPDT_DT").visible =  colVisible;
 
-    // setColumnProperty
+    toDoGridView.setColumnProperty("PROC_ID","width",80);
+    toDoGridView.setColumnProperty("PROC_NM","width",150);
+    toDoGridView.setColumnProperty("STEP_ID","width",50);
+    toDoGridView.setColumnProperty("TODO_ASK","width",400);
+    toDoGridView.setColumnProperty("TODO_ASNM","width",100);
+    toDoGridView.setColumnProperty("FRST_RGST_DT","width",100);
+    toDoGridView.setColumnProperty("WR_UNIT","width",60);
+    toDoGridView.setColumnProperty("COMPL_ST","width",60); // 완료여부
+    toDoGridView.setColumnProperty("FRST_RGSR_ID","width",100);
+
+    toDoGridView.onCellClicked = function (grid, clickData) {
+        const current = toDoGridView.getCurrent();
+        const values = toDoProvider.getJsonRow(current.dataRow)
+        const todoAsk = values.TODO_ASK;
+        
+        toDoAskEditor.setMarkdown(todoAsk);
+    }
 
 
+    axios.get(contextPath + '/Integration/SelectItgToDoList',{
+        params: {
+                   
+        }
+    })
+    .then(function(response) {
+        const list = response.data;
+		if(list.length != 0) {
+			toDoProvider.setRows(list);
+			// gridView.setFocus();
+		}
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
 }
 
 
@@ -215,6 +265,47 @@ function createTodoListGrid() {
 
 
 
+// Answer Grid
+let ansContainer, ansProvider, ansGridView;
 
+let ansFields = [
+    {fieldName:"ANS_NM", dataType:"text"},      // 프로세스 ID
+    {fieldName:"CNFM_DT", dataType:"date"},      // 프로세스 명
+    {fieldName:"CNFM_ST", dataType:"text"},      // No
+]
 
+let ansColumns = [
+	{fieldName:"ANS_NM", name:"ANS_NM", header:{text:"확인자"}},
+	{fieldName:"CNFM_DT", name:"CNFM_DT", header:{text:"확인날짜"}},
+    {fieldName:"CNFM_ST", name:"CNFM_ST", header:{text:"완료여부"}},
+];
+
+function createAnsGrid() {
+    ansContainer = document.getElementById('answerGrid');
+    ansProvider = new RealGrid.LocalDataProvider(false);
+    ansGridView = new RealGrid.GridView(ansContainer);
+    ansGridView.setDataSource(ansProvider);
+
+    ansProvider.setFields(ansFields);
+    ansGridView.setColumns(ansColumns);
+
+    ansGridView.displayOptions.rowHeight = 30;
+    ansGridView.header.height = 10;
+    ansGridView.footer.height = 10;
+    ansGridView.stateBar.width = 16;
+
+    ansGridView.setCheckBar({visible: false}); // 그리드 체크 박스 표시
+    ansGridView.setRowIndicator({visible: false}); // 그리드 행수 표시
+    ansGridView.setStateBar({visible: false}); // 그리드에서 입력,수정,삭제 표시
+    ansGridView.setDisplayOptions({focusVisible:false}); // 포커스 표시 여부
+    ansGridView.displayOptions.rowFocusType = "row"; // 그리드 한줄 선택
+
+    ansGridView.displayOptions.fitStyle = "evenFill"; // 그리드 꽉 채우기
+
+    // ReadOnly
+	// 그리드 컬럼들의 editable 상태를 편히 관리하기 위해 컬럼 마다 관리
+	ansColumns.forEach((element) => {
+		ansGridView.columnByName(element.name).editable = false;
+	});
+}
 
